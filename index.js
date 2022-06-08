dab8929b35b1fd5a0d7bdac375862c0ed75792f4
peter enejo <petstorm101@gmail.com>
2022-06-08 18:20:03 +0100

    commit tree 6669c397d839cf62dc6fd8aa9cdf63fc1037c2ff

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2; 
+
+let random = Math.floor((Math.random() * 10) + 1);
+
+let num3 = 44;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+
+let max = Math.max(5, 6, 4, 6, 12, 15, 20, 10);
+
